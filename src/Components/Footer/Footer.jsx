a7570e2139386857dc5930f56577d18ffe5906b0
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-02-17 23:22:34 +0200

    footerFixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from 'react-router';
 const Footer = () => {
   const navigate=useNavigate()
   return (
-    <div name="Footer" className='flex lg:justify-center relative bg-[#ebdae0] w-full h-[45rem] lg:h-[20rem]'>
+    <div name="Footer" className='flex lg:justify-center relative bg-[#ebdae0] w-full h-[45rem] lg:h-[22rem]'>
             <div className='relative w-[90%] flex  justify-center h-full ' >
-                <div className='w-[80%] mt-[4rem] flex flex-col items-center justify-between lg:h-full h-[80%] lg:grid lg:grid-cols-3'>
+                <div className='w-[80%] mt-[4rem] flex flex-col items-center justify-between  h-[80%] lg:h-full  lg:grid lg:grid-cols-3 lg:self-center'>
                      <div className='bg-cover bg-logo w-[120px] h-[96px]'  />
                      <div className='flex flex-col  '>
                         <h2 className=' lg:text-[20px]'>UTILE</h2>
@@ -48,7 +48,7 @@ const Footer = () => {
                             <li className='mt-2 cursor-pointer'>Contact</li>
                         </ul>
                      </div>
-                     <div className='flex flex-col '>
+                     <div className='flex flex-col ml-[5.7rem] lg:ml-0 '>
                      <h3 className='lg:text-[20px]'>CUVINTE CHEIE</h3>
                      <ul className='mt-[1rem]'>
                       <li>Cursuri unghii In Bucuresti</li>
@@ -57,8 +57,9 @@ const Footer = () => {
                       <li className='mt-2'>Nails training</li>
                      </ul>
                      </div>
+                     <h3 className='absolute bottom-2 ml-[2rem] lg:ml-0 text-[14px]' > @ 2023 CURSURI UNGHII IN BUCURESTI  ADINA HIRSCH</h3>
                 </div>
-                <h3 className='absolute bottom-0 ml-[1rem] lg:ml-0 text-[14px]' > @ 2023 CURSURI UNGHII IN BUCURESTI  ADINA HIRSCH</h3>
+                
             </div>
             
     </div>
